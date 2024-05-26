6ab3380f34710a97387a39d0f1c1479c68e032a2
ak-yoshimatsu <codetrain.akihiro.yoshimatsu@gmail.com>
2024-05-26 15:46:38 +0900

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+// import { Noto_Sans } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+// export const inter = Noto_Sans({ weight: '200', subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
