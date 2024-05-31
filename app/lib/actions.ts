0719c9472d77e43d5350fe85d4b1d61fb6f253a6
ak-yoshimatsu <codetrain.akihiro.yoshimatsu@gmail.com>
2024-05-31 23:59:12 +0900

    認証機能追加

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { sql } from '@vercel/postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -59,4 +61,23 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return '不正なログイン情報';
+        default:
+          return '何か悪いことが起こった';
+      }
+    }
+    throw error;
+  }
 }
